7c382775a03fa3f4509a3a18279944a1ffeea41b
Nam Nguyensource <dangnam2030@gmail.com>
2025-11-03 22:06:20 +0000

    Fix exporting

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-export * from './lib/request-schema.decorator';
-export * from './lib/response-schema.decorator';
-export * from './lib/app-result';
-export * from './lib/http-result';
+export { RequestSchema, InputData, } from "./lib/request-schema.decorator";
+export { ResponseSchema, OutputData } from "./lib/response-schema.decorator";
+export { AppResult } from "./lib/app-result";
+export { HttpResult } from "./lib/http-result";

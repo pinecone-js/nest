d9d3290c4b59c400b311967eb442dc07a8995d50
Nam Nguyensource <dangnam2030@gmail.com>
2025-11-14 04:19:10 +0000

    Fix configure

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export * from "./messaging/app-message";
 export * from "./messaging/http-output";
 
 export class Pinecone {
-  static configure(configs: Record<ConfigKey, any>): void {
+  static configure(configs: Partial<Record<ConfigKey, any>>): void {
     for (const [key, value] of Object.entries(configs)) {
       setConfig(key as ConfigKey, value);
     }
